852cee4337e95e6ee69ae3ed83f8448c1a3fe286
Laura Martens <laura.d.martens@icloud.com>
2024-11-05 22:04:30 +0100

    eqtl script

--- scripts/inference_eqtls/parallel_inference-susie_fixed.py
+++ scripts/inference_eqtls/parallel_inference-susie_fixed.py
@@ -2,19 +2,20 @@ import os
 
 import numpy as np
 import pandas as pd
-import polars as pl
 import scipy
-
 import torch
 import tqdm
 from accelerate import Accelerator
-from polya_project.data import GenomeIntervalDataset, str_to_one_hot
-from peft import LoraConfig, get_peft_model
+import polars as pl
+
 from torch.utils.data import DataLoader
 
-from utils.utils import fix_rev_comp_multiome, get_gene_slice_and_strand, get_cell_count_pred
-from modeling.scborzoi import ScBorzoi
-from data.scdata import onTheFlyExonMultiomePseudobulkDataset
+from enformer_pytorch.data import GenomeIntervalDataset, str_to_one_hot
+
+from scooby.modeling import Scooby
+from scooby.data import onTheFlyExonMultiomePseudobulkDataset
+from scooby.utils.utils import fix_rev_comp_multiome, get_cell_count_pred,get_gene_slice_and_strand
+from scooby.utils.transcriptome import Transcriptome
 
 def get_pred_for_snp(row):
     try:
@@ -26,7 +27,7 @@ def get_pred_for_snp(row):
         filter_val = lambda df: df.filter(pl.col('column_4').is_in(genes))#
         #filter_val = lambda df: df.filter(True)
         val_ds = GenomeIntervalDataset(
-            bed_file = os.path.join(data_path,'borzoi_training_data_fixed', 'train_val_test_gene_sequences.csv'),
+            bed_file = os.path.join(data_path,'scooby_training_data', 'train_val_test_gene_sequences.csv'),
             fasta_file = fasta_file,
             filter_df_fn = filter_val,
             return_seq_indices = False,
@@ -68,7 +69,6 @@ def get_pred_for_snp(row):
                 span=True
             )
             
-            bs = x[0].shape[0]
             seqs = x[0].cuda().permute(0,2,1)
         
             seqs_alt = seqs.clone()
@@ -94,46 +94,35 @@ def predict(model, seqs, seqs_rev_comp, conv_weights, conv_biases, bins_to_predi
             else:
                 outputs_rev_comp = model.forward_sequence_w_convs(seqs_rev_comp, conv_weights, conv_biases, bins_to_predict = None)
     flipped_version = torch.flip(outputs_rev_comp,(1,-3))
-    outputs_rev_comp = fix_rev_comp_multiome(flipped_version) #fix_rev_comp2(flipped_version)
-    #outputs_rev_comp = fix_rev_comp2(flipped_version) #fix_rev_comp2(flipped_version)
+    outputs_rev_comp = fix_rev_comp_multiome(flipped_version) 
     return (outputs + outputs_rev_comp)/2
 
 
 if __name__ == '__main__' :
     accelerator = Accelerator(step_scheduler_with_optimizer=False)
 
-    data_path = 'tmp'
-    csb = ScBorzoi(
-        cell_emb_dim=14, 
-        n_tracks=3,
-        embedding_dim=1920, 
-        return_center_bins_only=True, 
-        disable_cache=True,
-        use_transform_borzoi_emb=True
-    )
-    old_weights = torch.load('/s/project/QNA/borzoi/f0/model0_best.h5.pt')
-    csb.load_state_dict(old_weights, strict = False)
-    
-    config = LoraConfig(
-        target_modules=r"(?!separable\d+).*conv_layer|.*to_q|.*to_v|transformer\.\d+\.1\.fn\.1|transformer\.\d+\.1\.fn\.4",
-    )
-    csb = get_peft_model(csb, config)
-    csb.load_state_dict(torch.load(os.path.join(data_path, 'borzoi_saved_models/csb_epoch_20_scDog-neurips-PMseq-4nodrop-softclip5-64cell-normalizeATAC-fixedemb-noneighbors-rightembeddingrightsplit-longer/pytorch_model.bin'))) 
-    csb = csb.merge_and_unload()
+    data_path = '/data/ceph/hdd/project/node_08/QNA/scborzoi/submission_data'
+    csb = Scooby.from_pretrained(
+    'johahi/neurips-scooby',
+    cell_emb_dim=14,
+    embedding_dim=1920,
+    n_tracks=3,
+    return_center_bins_only=True,
+    disable_cache=False,
+    use_transform_borzoi_emb=True,
+)
     csb = accelerator.prepare(csb)
     csb.eval()
+    
     gtf_file = os.path.join(data_path, "gencode.v32.annotation.sorted.gtf.gz")
-    fasta_file = os.path.join(data_path, 'genome_human.fa')
-    bed_file = os.path.join(data_path, 'sequences.bed')
-    import pickle
-    with open(os.path.join(data_path, 'gencode.v32.annotation.gtf.transcriptome'), 'rb') as handle:
-        transcriptome = pickle.load(handle)
-    base_path = os.path.join(data_path, 'snapatac', 'pseudobulks/')
-    sample = 'merged'
-    neighbors_100 = scipy.sparse.load_npz(os.path.join(data_path, 'borzoi_training_data_fixed', 'neighbors_100_no_val_genes_new.npz'))
-    embedding = pd.read_parquet(os.path.join(data_path, 'borzoi_training_data_fixed',  'embedding_no_val_genes_new.pq'))
-    neighbors = scipy.sparse.csr_matrix(neighbors_100.shape)
-    cell_type_index = pd.read_parquet(os.path.join(data_path,  'borzoi_training_data_fixed/celltype_fixed.pq'))
+    fasta_file = os.path.join(data_path, "scooby_training_data", "genome_human.fa")
+    bed_file = os.path.join(data_path, "scooby_training_data", "sequences.bed")
+    transcriptome = Transcriptome(gtf_file)
+    base_path = os.path.join(data_path, 'scooby_training_data', 'pseudobulks')
+
+    neighbors = scipy.sparse.load_npz(os.path.join(data_path, 'scooby_training_data', 'no_neighbors.npz'))
+    embedding = pd.read_parquet(os.path.join(data_path, 'scooby_training_data',  'embedding_no_val_genes_new.pq'))
+    cell_type_index = pd.read_parquet(os.path.join(data_path,  'scooby_training_data', 'celltype_fixed.pq'))
     cell_type_index['size'] = cell_type_index['cellindex'].apply(lambda x: len(x))
     cell_type_index['celltype_name'] = cell_type_index['celltype'].copy()
     cell_type_index['celltype'] = cell_type_index['celltype'].str.replace(' ', '_').replace(r"G/M_prog", "G+M_prog").replace("MK/E_prog", "MK+E_prog")
@@ -148,7 +137,7 @@ if __name__ == '__main__' :
                 )
             ).cuda()
     conv_weights, conv_biases = csb.forward_cell_embs_only(embeddings.unsqueeze(0))
-    snp_df = pd.read_csv(os.path.join(data_path, "annotations/susie_snps_fixed.csv"))
+    snp_df = pd.read_csv(os.path.join(data_path, "eQTL_preds" , "susie_snps_fixed.csv"))
     snp_df['pred'] = snp_df.apply(lambda x : get_pred_for_snp(x), axis = 1)
     
-    snp_df.to_pickle(os.path.join("annotations/susie_snps_preds_fixed.pickle"))
+    # snp_df.to_pickle(os.path.join(data_path, "eQTL_preds" , "susie_snps_preds_fixed.pickle"))
